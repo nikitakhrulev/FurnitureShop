71c32d9e8486869f2e3e5fba14a6d96c6775dae1
Nikita <dubblerner.2@gmail.com>
2024-04-13 14:03:36 +0300

    swiper

--- js/main.js
+++ js/main.js
@@ -28,8 +28,25 @@ const swiper = new Swiper('.swiper', {
     
     loop: true,
 
+    freeMode: true,
+
     slidesPerView: 4,
     spaceBetween: 42,
+
+    // breakpoints: {
+    //     640: {
+    //       slidesPerView: 2,
+    //       spaceBetween: 20,
+    //     },
+    //     768: {
+    //       slidesPerView: 4,
+    //       spaceBetween: 40,
+    //     },
+    //     1024: {
+    //       slidesPerView: 5,
+    //       spaceBetween: 50,
+    //     },
+    //   },
 
     // Navigation arrows
     navigation: {
